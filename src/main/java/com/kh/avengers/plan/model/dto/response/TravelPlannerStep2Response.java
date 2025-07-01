b087f6e9a05b279d8e91a387d6f75100e9edc00f
SeungWon <semo970921@naver.com>
2025-07-01 09:40:55 +0900

    feat/planner2 requestData와 responseData DTO 생성

---
+++ src/main/java/com/kh/avengers/plan/model/dto/response/TravelPlannerStep2Response.java
@@ -0,0 +1,20 @@
+package com.kh.avengers.plan.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TravelPlannerStep2Response {
+
+    private Long planNo; 
+    private String selectedRegion;
+    private String message;
+    
+}
