be01326b0b5384491fcb5fa29f479817180cf185
SeungWon <semo970921@naver.com>
2025-07-01 12:34:53 +0900

    feat/planner step2 구현 완료 및 포스트맨 확인 완료

--- src/main/java/com/kh/avengers/plan/controller/TravelPlannerController.java
+++ src/main/java/com/kh/avengers/plan/controller/TravelPlannerController.java
@@ -3,6 +3,7 @@ package com.kh.avengers.plan.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kh.avengers.auth.model.vo.CustomUserDetails;
 import com.kh.avengers.common.dto.RequestData;
 import com.kh.avengers.plan.model.dto.request.TravelPlannerStep1Request;
+import com.kh.avengers.plan.model.dto.request.TravelPlannerStep2Request;
 import com.kh.avengers.plan.model.dto.response.TravelPlannerStep1Response;
+import com.kh.avengers.plan.model.dto.response.TravelPlannerStep2Response;
 import com.kh.avengers.plan.model.service.TravelPlannerStep1Service;
+import com.kh.avengers.plan.model.service.TravelPlannerStep2Service;
 import com.kh.avengers.util.ResponseUtil;
 
 import jakarta.validation.Valid;
@@ -25,6 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 public class TravelPlannerController {
 
     private final TravelPlannerStep1Service travelPlannerStep1Service;
+    private final TravelPlannerStep2Service travelPlannerStep2Service;
     private final ResponseUtil responseUtil;
     
     @PostMapping("/step1")
@@ -39,10 +44,25 @@ public class TravelPlannerController {
 
             log.info("여행 플래너 step1 생성 완료!!! >> 플랜 번호 : {}", response.getPlanNo());
 
-            RequestData result = responseUtil.rd("201", response, "여행 플래너" );
+            RequestData result = responseUtil.rd("201", response, "여행 플래너 생성" );
 
             return ResponseEntity.status(201).body(result);
         }
     
+        @PutMapping("/step2")
+        public ResponseEntity<RequestData> updateStep2Plan(
+            @RequestBody @Valid TravelPlannerStep2Request request,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+                
+                log.info("여행 플래너 step2 업데이트 요청 >> 시용자 : {}, 플랜번호 : {}, 선택지역 : {}", userDetails.getUsername(), request.getPlanNo(), request.getSelectedRegion());
+
+                TravelPlannerStep2Response response = travelPlannerStep2Service.updateStep2Plan(request,userDetails);
+
+                log.info("여행 플래너 step2 업데이트 완료!! >> 플랜번호 : {}, 선택지역 : {}",  response.getPlanNo(), response.getSelectedRegion());
+
+                RequestData result = responseUtil.rd("200", response, "여행 지역 선택 완료!!!");
+
+                return ResponseEntity.ok(result);
+            } 
 
 }
