4df9f3f34bc44d8cade239e80d412d4fc99370fd
kohyunwoo00 <hyunwoo36003@naver.com>
2025-07-02 09:23:53 +0900

    신고 작업중

---
+++ src/main/java/com/kh/report/controller/ReportController.java
@@ -0,0 +1,13 @@
+package com.kh.report.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/reports")
+public class ReportController {
+
+}
