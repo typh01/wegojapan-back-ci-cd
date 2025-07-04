1bca28962dc10e0eeb1313fad5e738d88b282a5d
SeungWon <semo970921@naver.com>
2025-07-04 23:58:41 +0900

    feat/내플랜 상세 조회

---
+++ src/main/java/com/kh/avengers/plan/myPlanDetail/model/dto/myPlanDetailDto.java
@@ -0,0 +1,32 @@
+package com.kh.avengers.plan.myPlanDetail.model.dto;
+
+import com.kh.avengers.plan.model.dto.SelectedPlaceDto;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class myPlanDetailDto {
+
+  private Long planNo;
+  private String planTitle;
+  private String planDescription;
+  private String planStatus;
+  private LocalDate travelStartDate;
+  private LocalDate travelEndDate;
+  private Integer groupSize;
+  private Long minBudget;
+  private Long maxBudget;
+  private String transportReservationLink;
+  private String accommodationLink;
+  private LocalDateTime createDate;
+  private LocalDateTime updateDate;
+  private List<SelectedPlaceDto> selectedPlaces;
+
+}
