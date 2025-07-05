5529ec348c4570627af099ffdc06b979be043fa9
SeungWon <semo970921@naver.com>
2025-07-05 12:03:23 +0900

    fea/플래너 상세정보 수정하기 구현 완료

--- src/main/java/com/kh/avengers/plan/myPlanDetail/controller/MyPlanDetailController.java
+++ src/main/java/com/kh/avengers/plan/myPlanDetail/controller/MyPlanDetailController.java
@@ -10,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -23,22 +20,47 @@ public class MyPlanDetailController {
 
   private final MyPlanDetailService myPlanDetailService;
   private final ResponseUtil responseUtil;
-
+  
+  // 플랜 상세정보 조회
   @GetMapping("/{planNo}")
   public ResponseEntity<RequestData> getPlanDetail(
           @PathVariable Long planNo,
           @AuthenticationPrincipal CustomUserDetails userDetails) {
     log.info("플랜 상세 조회 요청! >> 사용자 : {}, 플랜번호 : {}", userDetails.getMemberName(), planNo);
 
-    try{
+    try {
       MyPlanDetailDto planDetail = myPlanDetailService.getPlanDetail(planNo, userDetails);
       log.info("플랜 상세조회 성공!!! >> 사용자 : {}, 플랜번호 : {}, 플랜제목 : {}", userDetails.getMemberName(), planNo, planDetail.getPlanTitle());
       RequestData result = responseUtil.rd("200", planDetail, "플랜 상세 조회 성공!!!!");
       return ResponseEntity.ok(result);
-    } catch (ForbiddenException e){
+    } catch (ForbiddenException e) {
       log.warn("플랜 상세 조회 실패 >> 사용자 : {}, 플랜번호 : {}", userDetails.getMemberName(), planNo);
       RequestData errorResult = responseUtil.rd("404", null, e.getMessage());
       return ResponseEntity.status(404).body(errorResult);
+    }
+  }
+
+  // 플랜 상세정보 수정
+  @PutMapping("/{planNo}")
+  public ResponseEntity<RequestData> updatePlanDetail(
+          @PathVariable Long planNo,
+          @RequestBody MyPlanDetailDto planDetailDto,
+          @AuthenticationPrincipal CustomUserDetails userDetails) {
+    log.info("플랜 수정 요청! >> 사용자 : {}, 플랜번호 : {}", userDetails.getMemberName(), planNo);
+
+    try {
+      planDetailDto.setPlanNo(planNo);
+
+      MyPlanDetailDto updatedPlan = myPlanDetailService.updatePlanDetail(planDetailDto, userDetails);
+      log.info("플랜 수정 성공!!! >> 사용자 : {}, 플랜번호 : {}, 플랜제목 : {}", userDetails.getMemberName(), planNo, updatedPlan.getPlanTitle());
+
+      RequestData result = responseUtil.rd("200", updatedPlan, "플랜 수정 성공!!!!");
+      return ResponseEntity.ok(result);
+
+    } catch (ForbiddenException e) {
+      log.warn("플랜 수정 실패 >> 사용자 : {}, 플랜번호 : {}", userDetails.getMemberName(), planNo);
+      RequestData errorResult = responseUtil.rd("403", null, e.getMessage());
+      return ResponseEntity.status(403).body(errorResult);
     }
   }
 }
