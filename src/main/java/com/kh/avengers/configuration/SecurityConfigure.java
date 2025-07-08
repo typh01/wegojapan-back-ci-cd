bad2dde9941580dbcb883cf1f55280809e91eecf
SeungWon <semo970921@naver.com>
2025-07-08 11:15:09 +0900

    fix/소켓관련해서 설정 수정

--- src/main/java/com/kh/avengers/configuration/SecurityConfigure.java
+++ src/main/java/com/kh/avengers/configuration/SecurityConfigure.java
@@ -97,7 +97,12 @@ public class SecurityConfigure {
   @Bean
   public CorsConfigurationSource corsConfigurationSource(){
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+    // configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+    configuration.setAllowedOrigins(Arrays.asList(
+            "https://wegojapan.shop",
+            "http://localhost:5173"
+
+    ));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
     configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
     configuration.setAllowCredentials(true);
